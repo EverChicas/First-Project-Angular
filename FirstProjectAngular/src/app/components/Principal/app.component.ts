ef96da9b37a362861f95c37b63ddbf397b296681
EverChicas <everchicasp@gmail.com>
2020-07-01 10:58:48 -0600

    video 7

--- FirstProjectAngular/src/app/components/Principal/app.component.ts
+++ FirstProjectAngular/src/app/components/Principal/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'FirstProjectAngular';
+
+  usuarios = []
+
+  constructor(
+    private _http: HttpClient
+  ) {}
+
+  ngOnInit(){
+    this._http.get('http://jsonplaceholder.typicode.com/users')
+    .subscribe((datos: any[]) => this.usuarios = datos)
+  }
+
 }
